7e24c360d399ca32117f8c3d5a966ff3e9c935f0
Sultan <87053811230s@gmail.com>
2023-05-18 21:52:20 +0600

    151: Жизненный цикл компонентов

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -15,14 +15,25 @@ class MarvelService {
     }
 
 
-    getAllCharaters = () => {
-        return this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
-
+    getAllCharaters = async () => {
+        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+        return res.data.results.map(this._transformCharacter);
     }
     
-    getAllCharater = (id) => {
-        return this.getResource(`${this._apiBase}characters/${id}?&${this._apiKey}`);
-
+    getAllCharater = async (id) => {
+        const res = await this.getResource(`${this._apiBase}characters/${id}?&${this._apiKey}`);
+        return this._transformCharacter(res.data.results[0]);
+    }
+ 
+    _transformCharacter = (char) => {
+        return {
+            id: char.id,
+            name: char.name,
+            description: char.description ? `${char.description.slice(0, 210)}...` : 'This character has no description',
+            thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+            homepage: char.urls[0].url,
+            wiki: char.urls[1].url
+        }
     }
 }
 
